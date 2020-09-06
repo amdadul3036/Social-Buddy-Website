6397ac07950bd3cbdd07055c4d2573ce12b5da5f
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-06 09:26:44 -0700

    Done

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div style = {{textAlign:'center'}}>
+            <h6 > All Right reserved by MD AMDADUL ISLAM</h6>
+            <h6>Find me on <a href = "https://www.linkedin.com/in/mdamdadulislam/">Linkedin</a> and <a href = "https://www.instagram.com/dhruboish/?hl=en">Instagram</a>  </h6>
+        </div>
+    );
+};
+
+export default Footer;
